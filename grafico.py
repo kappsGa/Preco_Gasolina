85bd12a262dbc21af5918a59f3ea042683fe4193
KappsGa <Kapps.ga@gmail.com>
2024-06-18 16:29:53 +0000

    Preço da gasolina criado

--- grafico.py
+++ grafico.py
@@ -1,5 +1,8 @@
 import pandas as pd
 import seaborn as sns
+import matplotlib.pyplot as plt
 
 gasolina_df = pd.read_csv("gasolina.csv")
 sns.lineplot(data=gasolina_df, x="dia", y="venda")
+
+plt.savefig("gasolina.png")
